dbcf6b5550f48e0b7b7f2458fc1a9d06fdd801d2
LalitTarsariya <lalittarsariya37@gmail.com>
2016-01-20 18:06:48 +0530

    change chio16.py

--- chio16.py
+++ chio16.py
@@ -1,5 +1,8 @@
 data=[[1, 2, 1, 1],[1, 1, 4, 1],[1, 3, 1, 6],[1, 7, 2, 5]]
+"""
+cl=column length
+rl=row lwngth
+"""
 
-for i in len(data)
-	for j in len(data[0])
-		
+direction = [(dx, dy) for dy in range(-1, 2) for dx in range(-1, 2) if dx != 0 or dy != 0]
+
